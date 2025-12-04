7a758250b66e25bea00c61eee3a5d2ed91c891c6
Mihajlo Lukovic <lukovici@Mihajlos-MacBook-Pro.local>
2025-12-04 16:41:16 +0100

    Add TC details to the test_products_title failing test

--- tests/ui/test_failing.py
+++ tests/ui/test_failing.py
@@ -1,10 +1,23 @@
 from pages import Pages
 
 
-def test_failing_test(driver):
+def test_products_title(driver):
+    """Test accurate "Products" tab title text
+
+    Preconditions:
+        - User is on the “Products” page
+
+    Test steps:
+    1. Navigate on the "Products" page -> User is on the "Products" page
+    2. Check "Products" tab text -> "Products" tab should have correct text
+
+    Note: Failing test
+    """
     pages = Pages(driver=driver)
 
+    # 1. Navigate on the "Products" page -> User is on the "Products" page
     pages.home.navigate()
     pages.home.products.wait_to_be_displayed()
 
+    # 2. Check "Products" tab text -> "Products" tab should have correct text
     assert pages.home.products.text() == 'Test'
